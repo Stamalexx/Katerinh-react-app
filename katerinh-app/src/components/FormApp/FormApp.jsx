9b22b089a20670ce5f9fae066c0065abbe653773
Alexios Stamelos <stamelosalexios@gmail.com>
2025-11-10 12:24:26 +0200

    route start

---
+++ katerinh-app/src/components/FormApp/FormApp.jsx
@@ -0,0 +1,26 @@
+import style from "./FormApp.module.css";
+
+export default function FormApp() {
+  return (
+    <form className={style.formContainer}>
+    <div>
+      <h2>Contact Us</h2>
+      <p>Please fill out this form.</p>
+
+      <form>
+        <label>
+          Your Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Your Message:
+          <textarea name="message"></textarea>
+        </label>
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    </form>
+  );
+}
